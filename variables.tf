2704fcf779634a998be4a2970cd89da2114e9a4b
Christoph Hannebauer <christoph.hannebauer@glueckkanja.com>
2026-05-22 13:01:26 +0200

    break the cycle

--- variables.tf
+++ variables.tf
@@ -292,7 +292,6 @@ variable "subnet_appservices_address_prefix" {
   description = "CIDR address prefix for the App Services subnet (e.g., from IPAM). When null, the prefix is auto-calculated from vnet_address_space using cidrsubnet(). Ignored when create_networking is false."
 
   validation {
-    # Mirror of subnet_endpoints_address_prefix validation
     condition = var.subnet_appservices_address_prefix == null || (
       # Valid CIDR notation
       can(cidrhost(var.subnet_appservices_address_prefix, 0)) &&
@@ -303,21 +302,11 @@ variable "subnet_appservices_address_prefix" {
           length(var.vnet_address_space) > 0 &&
           # Subnet must be contained within the VNet
           can(cidrcontains(var.vnet_address_space[0], var.subnet_appservices_address_prefix)) &&
-          cidrcontains(var.vnet_address_space[0], var.subnet_appservices_address_prefix) &&
-          (
-            # No overlap check needed when the other subnet is auto-calculated
-            var.subnet_endpoints_address_prefix == null || (
-              # Subnets must not overlap: neither range contains the other
-              can(cidrcontains(var.subnet_appservices_address_prefix, var.subnet_endpoints_address_prefix)) &&
-              can(cidrcontains(var.subnet_endpoints_address_prefix, var.subnet_appservices_address_prefix)) &&
-              !cidrcontains(var.subnet_appservices_address_prefix, var.subnet_endpoints_address_prefix) &&
-              !cidrcontains(var.subnet_endpoints_address_prefix, var.subnet_appservices_address_prefix)
-            )
-          )
+          cidrcontains(var.vnet_address_space[0], var.subnet_appservices_address_prefix)
         )
       )
     )
-    error_message = "Must be a valid CIDR notation (e.g., 10.0.1.0/26). When create_networking is true, the prefix must be contained within vnet_address_space[0] and must not overlap subnet_endpoints_address_prefix."
+    error_message = "Must be a valid CIDR notation (e.g., 10.0.1.0/26). When create_networking is true, the prefix must be contained within vnet_address_space[0]."
   }
 }
 
